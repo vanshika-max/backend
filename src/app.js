d89ee352188cefd833d7050c709e7510b58eae15
vanshika-max <vanshikaparjapati1@gmail.com>
2024-05-25 12:14:42 +0530

    routes and controllers

--- src/app.js
+++ src/app.js
@@ -13,4 +13,9 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import userRouter from "./routes/user.routes.js"
+
+//routes declartion
+app.use("/api/v1/users",userRouter)
 export {app}
